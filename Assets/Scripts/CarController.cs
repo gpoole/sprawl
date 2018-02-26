012aa46bfe330ac95f9d36e9734c5974b9d49f33
Greg Poole <m4dm4n@gmail.com>
2018-02-26 14:50:51 +1100

    Add a more arcadey car controller

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,76 +2,39 @@
 using System;
 
 public class CarController : MonoBehaviour {
-    [Tooltip("Maximum steering angle of the wheels")]
-    public float maxAngle = 30f;
-    [Tooltip("Maximum torque applied to the driving wheels")]
-    public float maxTorque = 300f;
-    [Tooltip("Maximum brake torque applied to the driving wheels")]
-    public float brakeTorque = 30000f;
-    [Tooltip("If you need the visual wheels to be attached automatically, drag the wheel shape here.")]
-    public GameObject wheelShape;
 
-    [Tooltip("The vehicle's speed when the physics engine can use different amount of sub-steps (in m/s).")]
-    public float criticalSpeed = 5f;
-    [Tooltip("Simulation sub-steps when the speed is above critical.")]
-    public int stepsBelow = 5;
-    [Tooltip("Simulation sub-steps when the speed is below critical.")]
-    public int stepsAbove = 1;
+    [Range(0f, 50f)]
+    public float turningFactor = 10f;
 
-    [Range(0f, 10f)]
-    public float sidewaysExtremumSlip = 1;
+    [Range(0f, 50f)]
+    public float accelerationFactor = 10f;
 
-    [Range(0f, 10f)]
-    public float sidewaysAsymptoteSlip = 2;
+    [Range(0f, 50f)]
+    public float frictionFactor = 5f;
 
-    [Range(0f, 10f)]
-    public float sidewaysAsymptoteValue = 1;
+    void Start() { }
 
-    [Range(0f, 10f)]
-    public float sidewaysExtremumValue = 2;
-
-    [Range(0f, 10f)]
-    public float sidewaysStiffness = 1;
-
-    [Range(0f, 10f)]
-    public float forwardExtremumSlip = 1;
-
-    [Range(0f, 10f)]
-    public float forwardAsymptoteSlip = 2;
-
-    [Range(0f, 10f)]
-    public float forwardAsymptoteValue = 1;
-
-    [Range(0f, 10f)]
-    public float forwardExtremumValue = 2;
-
-    [Range(0f, 10f)]
-    public float forwardStiffness = 1;
-
-    public Transform centreOfMass;
-
-    private WheelCollider[] m_Wheels;
-
-    public float brakingMass;
-
-    public Material wheelTouchMaterial;
-
-    public Material wheelMaterial;
-
-    private float initialMass;
-
-    private float steerAngle;
-
-    // Find all the WheelColliders down in the hierarchy.
-    void Start() {
-
-        public float GetSpeed() {
-            return GetComponent<Rigidbody>().velocity.magnitude;
-        }
-
-        // This is a really simple approach to updating wheels.
-        // We simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero.
-        // This helps us to figure our which wheels are front ones and which are rear.
-        void Update() {
-        }
+    public float GetSpeed() {
+        return GetComponent<Rigidbody>().velocity.magnitude;
     }
+
+    void FixedUpdate() {
+        var transform = GetComponent<Transform>();
+        var colliderRb = GetComponent<Rigidbody>();
+        var acceleration = Input.GetAxis("Vertical");
+        var turning = Input.GetAxis("Horizontal");
+        colliderRb.AddRelativeTorque(Vector3.up * turning * colliderRb.velocity.normalized.magnitude * turningFactor);
+        if (acceleration > 0) {
+            colliderRb.AddForce(transform.forward.normalized * acceleration * accelerationFactor);
+        }
+
+        var velocityDirection = colliderRb.velocity.normalized;
+        var orientation = new Vector3(transform.right.x, 0, transform.right.z);
+        var frictionVector = new Vector3(-velocityDirection.x, 0, -velocityDirection.z);
+        var sidewaysness = Mathf.Abs(Vector3.Dot(velocityDirection, orientation));
+        Debug.Log(sidewaysness);
+        Debug.Log("orientation=" + orientation);
+        Debug.Log("velocity=" + velocityDirection);
+        colliderRb.AddForce(frictionVector * colliderRb.velocity.magnitude * sidewaysness * frictionFactor);
+    }
+}
--- Assets/Scripts/UI/Speedo.cs
+++ Assets/Scripts/UI/Speedo.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 
 public class Speedo : MonoBehaviour {
 
-	public CarController car;
+    public CarController car;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		GetComponent<Text>().text = Mathf.Round(car.GetSpeed()) + " km/h";
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // GetComponent<Text>().text = Mathf.Round(car.GetSpeed()) + " km/h";
+    }
 }
