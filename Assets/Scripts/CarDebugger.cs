fb86414d415cf0b6ca342d3b0403efa002353970
Greg Poole <m4dm4n@gmail.com>
2018-02-28 16:54:42 +1100

    Add some warping code

--- Assets/Scripts/CarDebugger.cs
+++ Assets/Scripts/CarDebugger.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class CarDebugger : MonoBehaviour {
 
+    Vector3 initialPosition;
+
+    Quaternion initialRotation;
+
+    public Transform[] warpPoints;
+
+    void Start() {
+        initialPosition = GetComponent<Transform>().position;
+        initialRotation = GetComponent<Transform>().rotation;
+    }
+
     void Update() {
+        var rb = GetComponent<Rigidbody>();
+        var t = GetComponent<Transform>();
+
         if (Input.GetKeyUp(KeyCode.B)) {
-            var rb = GetComponent<Rigidbody>();
-            var t = GetComponent<Transform>();
             var mesh = GetComponent<BoxCollider>();
             rb.AddExplosionForce(750f, t.TransformPoint(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)) + (Vector3.down * 2f), 50f);
         }
+
+        if (Input.GetKeyUp(KeyCode.R)) {
+            WarpTo(initialPosition, initialRotation);
+        }
+
+        for (var i = 0; i < warpPoints.Length; i++) {
+            if (Input.GetKeyUp(i.ToString())) {
+                WarpTo(warpPoints[i].position, warpPoints[i].rotation);
+            }
+        }
+    }
+
+    void WarpTo(Vector3 position, Quaternion rotation) {
+        var t = GetComponent<Transform>();
+        var rb = GetComponent<Rigidbody>();
+        t.position = position;
+        t.rotation = rotation;
+        rb.velocity = Vector3.zero;
     }
 }
