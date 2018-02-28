780531b76f6bdfae21c3ed8813d4930bb39f178f
Greg Poole <m4dm4n@gmail.com>
2018-02-28 16:41:40 +1100

    Add a little explosion button

---
+++ Assets/Scripts/CarDebugger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDebugger : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetKeyUp(KeyCode.B)) {
+            var rb = GetComponent<Rigidbody>();
+            var t = GetComponent<Transform>();
+            var mesh = GetComponent<BoxCollider>();
+            rb.AddExplosionForce(750f, t.TransformPoint(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)) + (Vector3.down * 2f), 50f);
+        }
+    }
+}
