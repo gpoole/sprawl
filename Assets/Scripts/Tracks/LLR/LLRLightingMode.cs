5849740766631c79c2e879b7e31052642f6885ae
Greg Poole <m4dm4n@gmail.com>
2018-05-11 13:26:33 +1000

    Different scene lighting settings

--- Assets/Scripts/Tracks/LLR/LLRLightingMode.cs
+++ Assets/Scripts/Tracks/LLR/LLRLightingMode.cs
@@ -3,33 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class LLRLightingMode : MonoBehaviour {
+
+	public Light dayLight;
+
+	public Light nightLight;
+
+	public Material daySkybox;
+
+	public Material nightSkyBox;
+
+	public MeshRenderer water;
+
+	public Material dayWater;
+
+	public Material nightWater;
 
 	public enum Mode {
 		Daytime,
 		Evening,
 	}
 
-	public Mode mode {
-		get {
-			if (!_mode.HasValue) {
-				try {
-					_mode = (Mode) Enum.Parse(typeof(Mode), TrackManager.Instance.GetSetting("lighting"));
-				} catch (ArgumentException) {
-					return Mode.Daytime;
-				}
-			}
-			return _mode.Value;
-		}
-	}
-
-	private Mode? _mode;
+	public Mode mode;
 
 	void Start() {
-
+		if (TrackManager.Instance != null) {
+			try {
+				mode = (Mode) Enum.Parse(typeof(Mode), TrackManager.Instance.GetSetting("lightingMode"));
+			} catch (ArgumentException) {
+				Debug.LogError("Failed to parse lightingMode setting.");
+			}
+		}
+		ApplyMode();
 	}
 
-	void Update() {
+	void OnValidate() {
+		ApplyMode();
+	}
 
+	void ApplyMode() {
+		if (mode == Mode.Daytime) {
+			nightLight.gameObject.SetActive(false);
+			dayLight.gameObject.SetActive(true);
+			RenderSettings.skybox = daySkybox;
+			water.material = dayWater;
+		} else {
+			RenderSettings.skybox = nightSkyBox;
+			nightLight.gameObject.SetActive(true);
+			dayLight.gameObject.SetActive(false);
+			water.material = nightWater;
+		}
 	}
 }
